10a0d4ad607809c8403ed4c6c37f8a1d6e19e1fa
William F <60886603+wtcf90@users.noreply.github.com>
2020-08-02 15:37:57 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,20 @@
 function renderPlots(id){
   d3.json("samples.json").then (sampledata =>{
     var ids = sampledata.samples[0].otu_ids;
-    var datasamples = sampleData.samples[0].sample_values.slice(0,10).reverse();
+    var datasamples = sampledata.samples[0].sample_values.slice(0,10).reverse();
     var datalabels =  sampledata.samples[0].otu_labels.slice(0,10);
-    var OTUtop = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
-    var OTU_id = OTU_top.map(d => "OTU " + d);
+    var otu_top = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+    var otu_ids = otu_top.map(d => "OTU " + d);
+    // var datalabels =  sampledata.samples[0].otu_labels.slice(0,10);
+    var trace1 = {
+      x: datasamples,
+      y: OTU_id,
+      marker: {
+      color: 'blue'},
+      type:"bar",
+      orientation: "h",
+      text: datalabels,
+    };
+  var data = [trace1];
   })
 }
